79504d89e82df8db8dce7bc208b55949b374ad9c
Acer <Acer@DESKTOP-9FQK3MG>
2019-06-10 19:26:01 -0300

    Creacion clase user con sus metodos accesores y su contructor parametrizado.

--- src/java/aplicacion/modelo/dominio/User.java
+++ src/java/aplicacion/modelo/dominio/User.java
@@ -5,10 +5,128 @@
  */
 package aplicacion.modelo.dominio;
 
+import java.util.Calendar;
+
 /**
  *
  * @author LILI-PC
  */
 public class User {
+    private int ID;
+    private String Surname;
+    private String Name;
+    private int DNI;
+    private String Email;
+    private Calendar DateOfBirth;
+    private String Password;
+
+    public User(int ID, String Surname, String Name, int DNI, String Email, Calendar DateOfBirth, String Password) {
+        this.ID = ID;
+        this.Surname = Surname;
+        this.Name = Name;
+        this.DNI = DNI;
+        this.Email = Email;
+        this.DateOfBirth = DateOfBirth;
+        this.Password = Password;
+    }
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the Surname
+     */
+    public String getSurname() {
+        return Surname;
+    }
+
+    /**
+     * @param Surname the Surname to set
+     */
+    public void setSurname(String Surname) {
+        this.Surname = Surname;
+    }
+
+    /**
+     * @return the Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     * @param Name the Name to set
+     */
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    /**
+     * @return the DNI
+     */
+    public int getDNI() {
+        return DNI;
+    }
+
+    /**
+     * @param DNI the DNI to set
+     */
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+
+    /**
+     * @return the Email
+     */
+    public String getEmail() {
+        return Email;
+    }
+
+    /**
+     * @param Email the Email to set
+     */
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    /**
+     * @return the DateOfBirth
+     */
+    public Calendar getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    /**
+     * @param DateOfBirth the DateOfBirth to set
+     */
+    public void setDateOfBirth(Calendar DateOfBirth) {
+        this.DateOfBirth = DateOfBirth;
+    }
+
+    /**
+     * @return the Password
+     */
+    public String getPassword() {
+        return Password;
+    }
+
+    /**
+     * @param Password the Password to set
+     */
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
     
 }
